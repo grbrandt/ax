93d36c1fd622c17ef883c112e49328aae4e375f4
Geir Brandt <geir.rune.brandt@marel.com>
2025-01-29 14:38:59 +0100

    Removed copyright from csproj Restructured source files

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using Ax.Arguments;
-using Ax.Operations;
-using CommandLine;
-
-Parser.Default.ParseArguments<PackArguments, UnpackArguments>(args)
-    .WithParsed<UnpackArguments>(Unpack.Execute)
-    .WithParsed<PackArguments>(Pack.Execute);
---
+++ src/Arguments/UnpackArguments.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Ax.Arguments;
+
+[Verb("unpack", HelpText = "Extract archive")]
+public class UnpackArguments
+{
+    [Option('o', "output", Required = true, HelpText = "Destination folder")]
+    public string OutputFolder { get; set; } = "";
+
+    [Value(index: 0, Required = true, HelpText = "File to unpack")]
+    public string InputFile { get; set; } = "";
+
+    [Usage(ApplicationAlias = "ax")]
+    public static IEnumerable<Example> Examples
+    {
+        get
+        {
+            yield return new Example("Extract input file to current working folder", new UnpackArguments { InputFile = "c:\\temp\\inputfile.axz" });
+            yield return new Example("Extract input file to specified output folder", new UnpackArguments { InputFile = "c:\\temp\\inputfile.axz", OutputFolder  = "C:\\temp\\output"});
+        }
+    }
+}
